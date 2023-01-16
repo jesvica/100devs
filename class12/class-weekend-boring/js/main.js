7a4e3c26052df56dd5324faa928495fb2034f28f
Jessica Williams <jvictoriar@hotmail.co.uk>
2023-01-16 09:24:50 +0000

    removing case sensitivity from if statement

--- class12/class-weekend-boring/js/main.js
+++ class12/class-weekend-boring/js/main.js
@@ -4,11 +4,12 @@ function check() {
 
   const day = document.querySelector('#day').value
 
+
   //Conditionals go here
-if (day == "Tuesday".toLowerCase() || day == "Thursday".toLowerCase()) {
+if (day == "Tuesday" || day == "Thursday" || day == "Tuesday".toLowerCase() || day == "Tuesday".toUpperCase() || day == "Thursday".toLowerCase() || day == "Thursday".toUpperCase()) {
   console.log("It's a class day")
   document.getElementById("placeToSee").innerHTML = "It's a class day!";
-} else if (day == "saturday" || day == "sunday") {
+} else if (day == "Saturday" || day == "Sunday" || day == "Saturday".toUpperCase() || day == "Saturday".toLowerCase() || day == "Sunday".toUpperCase() || day == "Sunday".toLowerCase()) {
   console.log("It's the weekend!")
   document.getElementById("placeToSee").innerHTML = "It's the weekend!";
 } else {
