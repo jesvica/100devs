ca8fa468f0c5fe325d2742bf64491363c5d628b4
jessica <jvictoriaw@hotmail.co.uk>
2023-02-01 21:55:48 +0000

    modifying temp converter

--- class13/temperature-converter/js/main.js
+++ class13/temperature-converter/js/main.js
@@ -1,1 +1,11 @@
 //Write your pseduo code first! 
+
+// get the temperature in celcius, convert it to farenheit and show it on the screen
+
+function temperatureConverter() {
+    
+}
+
+let temperature = (celcius * 1.8) + 32;
+
+document.querySelector
