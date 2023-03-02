8ee26515a775ab1b4788455cc865f4b0f9992969
jessica <jvictoriaw@hotmail.co.uk>
2023-03-02 13:21:01 +0000

    recalling temp converter code

--- class14/temperature-converter/js/main.js
+++ class14/temperature-converter/js/main.js
@@ -1,1 +1,14 @@
 //Write your pseduo code first! 
+// I want to convert the temperature from celcius to farenheit
+// get the temp in celcius, convert it when the button is pressed, print answer to the screen
+
+document.querySelector('#yell').addEventListener('click', convert);
+
+function convert() {
+    //get the temp in celcius
+    let temp = document.querySelector('#celcius').value
+    //convert celcius to farenheit
+    temp = (temp * 9/5) + 32;
+    // print it to the screen
+    document.querySelector('#placeToYell').innerHTML = temp;
+}
