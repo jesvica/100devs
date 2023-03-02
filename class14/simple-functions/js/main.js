7e61e55a328372514ca400e286ee8fae7cb920db
jessica <jvictoriaw@hotmail.co.uk>
2023-03-02 20:20:43 +0000

    class function practice

--- class14/simple-functions/js/main.js
+++ class14/simple-functions/js/main.js
@@ -1,5 +1,22 @@
 //create a function that adds two numbers and alerts the sum
+function add(num1,num2){
+    alert(num1 + num2);
+};
+
+add(1,2);
 
 //create a function that multiplys three numbers and console logs the product
+function multiply(num1,num2,num3) {
+    // another way or writing, good for readability
+    // const product = num1 * num2 * num3 
+    console.log(num1 * num2 * num3)
+}
+
+multiply(3,4,5)
 
 //create a function that divides two numbers and returns the ???
+function divide(num1,num2) {
+    //return does not print to the console etc. must console log
+    return num1 / num2
+}
+console.log(divide(50,5))
