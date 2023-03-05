1d6a9b404d68f78fecce18c5d4d741b89ab1adbe
jessica <jvictoriaw@hotmail.co.uk>
2023-03-05 19:34:41 +0000

    completed tasks 1, 2, 3 & 4

--- class14/bring-it-home-practice/js/main.js
+++ class14/bring-it-home-practice/js/main.js
@@ -1,14 +1,37 @@
 // *Variables*
 // Create a variable and console log the value
+var word = 'hello'
+console.log(word)
 
 // Create a variable, add 10 to it, and alert the value
+let number = 5;
+number = number + 10;
+alert(number)
 
 // *Functions*
 // Create a function that subtracts 4 numbers and alerts the difference
+function subtractFourNums(n1,n2,n3,n4) {
+
+}
 
 // Create a function that divides one number by another and returns the remainder
+function dividesOneNumber(num1,num2) {
+    return num1 % num2
+}
+console.log(dividesOneNumber(50,4))
 
 // *Conditionals*
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
+function addTwoNums(no1,no2) {
+    addTwoNums = no1 + no2;
+
+    if (addTwoNums > 50) {
+        alert('Jumanji!')
+    }
+}
+addTwoNums(49, 31)
 
 // Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
+function multiplyThreeNums(m1,m2,m3) {
+    multiplyThreeNums = m1 * m2 * m3;
+}
