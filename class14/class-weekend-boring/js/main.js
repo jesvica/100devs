77b9be18311a5de1403cbc88580aafbee73a44fb
jessica <jvictoriaw@hotmail.co.uk>
2023-03-09 20:21:01 +0000

    recall practice

--- class14/class-weekend-boring/js/main.js
+++ class14/class-weekend-boring/js/main.js
@@ -1,17 +1,34 @@
 //Handle Capital Letters, Place result in DOM, add a check for humpday (Wed)
 
+// document.querySelector('#check').addEventListener('click', check)
+
+// function check() {
+
+//   const day = document.querySelector('#day').value
+
+//   if(day === "tuesday" || day === "thursday"){
+//     console.log("YOU HAVE CLASS")
+//   }else if( day === "saturday" || day === "sunday"){
+//     console.log("Its The Weekend")
+//   }else{
+//     console.log("BORING")
+//   }
+
+// }
+
 document.querySelector('#check').addEventListener('click', check)
 
 function check() {
 
-  const day = document.querySelector('#day').value
+  const day = document.querySelector('#day').value.toLowerCase()
 
-  if(day === "tuesday" || day === "thursday"){
-    console.log("YOU HAVE CLASS")
-  }else if( day === "saturday" || day === "sunday"){
-    console.log("Its The Weekend")
-  }else{
-    console.log("BORING")
+  if(day === 'tuesday' || day === 'thursday') {
+    document.querySelector('#placeToSee').innerHTML = 'YOU HAVE CLASS'
+  } else if (day === 'saturday' || day === 'sunday') {
+    document.querySelector('#placeToSee').innerHTML = 'ITS THE WEEKEND'
+  } else if (day === 'wednesday') {
+    document.querySelector('#placeToSee').innerHTML = 'HUMP DAY'
+  } else {
+    document.querySelector('#placeToSee').innerHTML ='BORINGGGGG'
   }
-
 }
