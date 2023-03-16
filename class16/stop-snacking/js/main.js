3fa5117754418b68ab64da735b1b0fdc8c86a112
jessica <jvictoriaw@hotmail.co.uk>
2023-03-16 22:56:45 +0000

    function and loop practice

--- class16/stop-snacking/js/main.js
+++ class16/stop-snacking/js/main.js
@@ -1,1 +1,16 @@
 //Create a function that grabs the number of snacks from the input and tells you to stop that many times
+
+document.querySelector('#help').addEventListener('click', stopSnacking)
+
+function stopSnacking() {
+    let numOfSnacks = Number(document.querySelector('input').value) //grabbing num of snacks from input
+    
+    document.querySelector('#stops').innerText = "" //empties the text each time #help runs
+
+    for(let i = 1; i <= numOfSnacks; i++) {
+        document.querySelector('#stops').innerText += ' Stop'
+    }
+}
+
+
+// document.getElementById('#stops').innerText += 'Stop'
