098bb7fb200e591c87fb7fe7ae6adf3f24a7534f
jessica <jvictoriaw@hotmail.co.uk>
2023-03-16 21:23:38 +0000

    for loop task

--- class16/sir-savage-the-21st/js/main.js
+++ class16/sir-savage-the-21st/js/main.js
@@ -1,2 +1,14 @@
 //Create a function that has a loop that prints '21' 21 times to the console and then call that function
+// for(let i=21; i<=21;i++) {
+//     console.log(i)
+// }
+
+ function twentyone() {
+        for(let i=1; i<=21;i++) {
+        document.querySelector('#savageSays').innerText += ' 21'
+    }
+ }
+twentyone()
+
 //Bonus can you make it print '21' 21 times to the dom?
+//document.querySelector('#savageSays').innerHTML
