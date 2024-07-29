bf7aacb0ee2d96ebfab64f26504a675a035a95d7
Jessica Williams <jvictoriaw@hotmail.co.uk>
2024-07-29 08:28:47 +0100

    updated to remove case sensitivity

--- javascript/class12-materials/class-weekend-boring/js/main.js
+++ javascript/class12-materials/class-weekend-boring/js/main.js
@@ -2,7 +2,7 @@ document.querySelector('#check').addEventListener('click', check)
 
 function check() {
 
-  const day = document.querySelector('#day').value
+  const day = document.querySelector('#day').value.toLowerCase();
 
   //Conditionals go here
 if (day == 'tuesday' || day == 'thursday') {
