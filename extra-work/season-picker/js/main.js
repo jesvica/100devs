de1713cb13ea04bdf565b1a2ee0cef4d56836f10
Jessica Williams <jvictoriaw@hotmail.co.uk>
2024-07-29 21:00:13 +0100

    added functions to change the background image when the list item with the correlating id is clicked

--- extra-work/season-picker/js/main.js
+++ extra-work/season-picker/js/main.js
@@ -1,1 +1,20 @@
+document.getElementById('spring').onclick = makeSpring
+document.getElementById('summer').onclick = makeSummer
+document.getElementById('autumn').onclick = makeAutumn
+document.getElementById('winter').onclick = makeWinter
 
+function makeSpring() {
+    document.querySelector('body').style.backgroundImage = "url(images/spring-img.jpg)"
+}
+
+function makeSummer() {
+    document.querySelector('body').style.backgroundImage = "url(images/summer-img.jpg)"
+}
+
+function makeAutumn() {
+    document.querySelector('body').style.backgroundImage = "url(images/autumn-img.jpg)"
+}
+
+function makeWinter() {
+    document.querySelector('body').style.backgroundImage = "url(images/winter-img.jpg)"
+}
