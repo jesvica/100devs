a679ed2e098fef86a8c6ee5529eba56d4c609c7e
Jessica Williams <jvictoriaw@hotmail.co.uk>
2024-07-29 08:38:01 +0100

    added colour yellow to the youtube background picker and modified css to match

--- javascript/class12-materials/background-picker/js/main.js
+++ javascript/class12-materials/background-picker/js/main.js
@@ -1,6 +1,7 @@
 document.getElementById('purple').onclick = partyPurple
 document.getElementById('green').onclick = partyGreen
 document.getElementById('blue').onclick = partyBlue
+document.getElementById('yellow').onclick = partyYellow
 
 
 function partyPurple() {
@@ -16,4 +17,9 @@ function partyGreen() {
 function partyBlue() {
   document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
   document.querySelector('body').style.color = 'white'
+}
+
+function partyYellow() {
+  document.querySelector('body').style.backgroundColor = 'rgb(255, 255, 0)'
+  document.querySelector('body').style.color = 'black'
 }
