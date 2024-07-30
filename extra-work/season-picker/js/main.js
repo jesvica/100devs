55e0a7cbe999bfb1ca8ccffc935b7b02d78303f6
Jessica Williams <jvictoriaw@hotmail.co.uk>
2024-07-30 07:59:51 +0100

    added default background image and reset option to reset the images back to default

--- extra-work/season-picker/js/main.js
+++ extra-work/season-picker/js/main.js
@@ -2,6 +2,7 @@ document.getElementById('spring').onclick = makeSpring
 document.getElementById('summer').onclick = makeSummer
 document.getElementById('autumn').onclick = makeAutumn
 document.getElementById('winter').onclick = makeWinter
+document.getElementById('reset').onclick = reset
 
 function makeSpring() {
     document.querySelector('body').style.backgroundImage = "url(images/spring-img.jpg)"
@@ -17,4 +18,8 @@ function makeAutumn() {
 
 function makeWinter() {
     document.querySelector('body').style.backgroundImage = "url(images/winter-img.jpg)"
+}
+
+function reset() {
+    document.querySelector('body').style.backgroundImage = "url(images/gradient.jpg)"
 }
