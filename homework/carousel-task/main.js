42811c92cc0f3bdbb713e4f7ee1d1520baa5e070
Jessica Williams <jvictoriaw@hotmail.co.uk>
2024-08-05 20:46:12 +0100

    completed first carousel task that prints odd numbers

--- homework/carousel-task/main.js
+++ homework/carousel-task/main.js
@@ -1,5 +1,13 @@
-let turns = prompt("How many turns?")
+// Carousel task
+// let turns = prompt("How many turns?")
 
-for (i = 1; i <= turns; i++) {
-    console.log(`Carousel turn: ${i}`)
-}
+// for (i = 1; i <= turns; i++) {
+//     console.log(`Carousel turn: ${i}`)
+// }
+
+// Parity task
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 !== 0) {
+      console.log(`${i} is odd`);
+    }
+  }
