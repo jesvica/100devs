84401c2441a2e7c859133b4090a88832e8f89d2b
Jessica Williams <jvictoriaw@hotmail.co.uk>
2024-08-05 20:48:01 +0100

    improved to show both odd and even numbers

--- homework/carousel-task/main.js
+++ homework/carousel-task/main.js
@@ -7,7 +7,9 @@
 
 // Parity task
 for (let i = 1; i <= 10; i++) {
-    if (i % 2 !== 0) {
-      console.log(`${i} is odd`);
+    if (i % 2 === 0) {
+      console.log(`${i} is even`);
+    } else if (i % 2 !== 0) {
+        console.log(`${i} is odd`)
     }
   }
