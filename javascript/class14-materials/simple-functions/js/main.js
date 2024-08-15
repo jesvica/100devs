84454b8058e0df3e3ba7b576ba72e9cb395de083
Jessica Williams <jvictoriaw@hotmail.co.uk>
2024-08-15 19:26:40 +0100

    completed bachelor code task 3 and simple functions task

--- javascript/class14-materials/simple-functions/js/main.js
+++ javascript/class14-materials/simple-functions/js/main.js
@@ -1,5 +1,15 @@
 //create a function that adds two numbers and alerts the sum
-
+function add(a,b) {
+    alert(a + b);
+}
+add(1,2)
 //create a function that multiplys three numbers and console logs the product
-
+function multiply(c,d,e) {
+    console.log(c * d * e)
+}
+multiply(5,2,3)
 //create a function that divides two numbers and returns the ???
+function divide(num1,num2) {
+    return num1 / num2
+}
+console.log(divide(10,5))
