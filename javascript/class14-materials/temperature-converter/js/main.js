a52c16adeb68952233ae50b11978de16ce3206fe
Jessica Williams <jvictoriaw@hotmail.co.uk>
2024-08-15 19:54:30 +0100

    practicing the temperature converter function, hid my previous code and rewrote without looking

--- javascript/class14-materials/temperature-converter/js/main.js
+++ javascript/class14-materials/temperature-converter/js/main.js
@@ -1,13 +1,29 @@
-//Write your pseduo code first!
+// //Write your pseduo code first!
+// document.querySelector('#convert').addEventListener('click', convert)
+
+// function convert() {
+//     // get the temperature in celsius
+//     let temp = document.querySelector('#celsius').value
+
+//     // convert it to fahrenheit
+//     temp = temp * 9/5 + 32;
+
+//     //print it to dom
+//     document.querySelector('#farenheit').innerHTML = temp;
+// }
+
+
+
+
+
 document.querySelector('#convert').addEventListener('click', convert)
 
 function convert() {
-    // get the temperature in celsius
-    let temp = document.querySelector('#celsius').value
+    // getting the input
+    let celc = document.querySelector('#celsius').value
 
-    // convert it to fahrenheit
-    temp = temp * 9/5 + 32;
-
-    //print it to dom
-    document.querySelector('#farenheit').innerHTML = temp;
+    //do the math
+    let fahren = (celc * 9/5) + 32;
+    //show it in the dom
+    document.querySelector('#farenheit').innerHTML = fahren;
 }
