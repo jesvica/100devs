76f85fe7c32964d32c97cdc7b161634c6deff640
Jessica Williams <jvictoriaw@hotmail.co.uk>
2024-09-06 08:39:31 +0100

    completed variable task

--- javascript/class16-materials/variable-fun-time/js/main.js
+++ javascript/class16-materials/variable-fun-time/js/main.js
@@ -1,18 +1,23 @@
 //--- Easy
 //create a variable and assign it a number
-
+let number = 27
 //minus 10 from that number
-
+number -= 10;
 //print that number to the console
-
+console.log(number)
 //--- Medium
 //create a variable that holds a value from the input
-
+let numFromInput = document.querySelector('#danceDanceRevolution').value
 //add 25 to that number
-
+numFromInput += 25
 //alert that number
-
+alert(numFromInput)
 //--- Hard
 //create a variable that holds the h1
-
+let holder = document.querySelector('h1')
 //add an event listener to that element that console logs the sum of the two previous variables
+holder.addEventListener('click', logSum)
+
+function logSum() {
+    console.log(number + Number(numFromInput))
+}
