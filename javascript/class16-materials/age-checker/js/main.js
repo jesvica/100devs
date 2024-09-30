66c44dea700f88eaec1d0c09429c42c28399d2c3
Jessica Williams <jvictoriaw@hotmail.co.uk>
2024-09-30 20:57:14 +0100

    completed the if statement section of the age-checker task

--- javascript/class16-materials/age-checker/js/main.js
+++ javascript/class16-materials/age-checker/js/main.js
@@ -1,4 +1,19 @@
 //Create a conditonal that checks their age
+let age = 25;
+
+if (age < 16) {
+    return "You can't drive";
+} else if (age < 18) {
+    return "You can't hate from outside the club, you can't even get in";
+} else if (age < 21) {
+    return "You can't drink";
+} else if (age < 25) {
+    return "You cannot rent cars affordably";
+} else if (age < 30) {
+    return "You can't rent fancy cars affordably";
+} else if (age >= 30) {
+    return "You have nothing else to look forward to";
+}
 //If under 16, tell them they can not drive
 //If under 18, tell them they can't hate from outside the club, because they can't even get in
 //If under 21, tell them they can not drink
@@ -11,3 +26,10 @@
 //On click of the h1
 //Take the value from the input
 //Place the result of the conditional in the paragraph
+
+document.querySelector('h1').addEventListener('click', takeValue);
+
+function takeValue() {
+    let valOfInput = document.querySelector('#danceDanceRevolution').value;
+    
+}
