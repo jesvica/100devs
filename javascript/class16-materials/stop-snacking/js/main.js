204584a8719aa99a3a390f59a4e051ff20f05624
Jessica Williams <jvictoriaw@hotmail.co.uk>
2024-10-01 21:51:01 +0100

    stop snacking task - using functions and loops

--- javascript/class16-materials/stop-snacking/js/main.js
+++ javascript/class16-materials/stop-snacking/js/main.js
@@ -1,1 +1,17 @@
 //Create a function that grabs the number of snacks from the input and tells you to stop that many times
+document.querySelector('#help').addEventListener('click', stopSnacking);
+
+function stopSnacking() {
+    const stopTimes = Number(document.querySelector('input').value); 
+    // Number ensures the input value is a number not a string
+    // VALUE not innerText as value is the num
+
+    // sets the inner text to be empty each time it's clicked, so we do not concatenat the results
+    document.querySelector('#stops').innerHTML = '';
+
+    for (let i = 1; i <= stopTimes; i++) {
+        document.querySelector('#stops').innerHTML += ' Stop'
+    }
+
+}
+
